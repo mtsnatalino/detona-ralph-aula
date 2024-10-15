56eaaba84419991e09e448fe7bbd2058b6e9933f
mtsnatalino <matheuspedro982@gmail.com>
2024-10-14 22:16:01 -0300

    implementando o listener de hitbox

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -10,7 +10,9 @@ const state = {
     },
     values: {
         timerId: null,
-        gameVelocity: 1000
+        gameVelocity: 1000,
+        hitPosition: 0,
+        result: 0,
     },
 };
 
@@ -22,6 +24,7 @@ function randomSquare(){
     let randomNumber = Math.floor(Math.random() * 9);
     let randomSquare = state.view.squares[randomNumber];
     randomSquare.classList.add("enemy");
+    state.values.hitPosition = randomSquare.id;
 }
 
 function moveEnemy(){
@@ -30,12 +33,20 @@ function moveEnemy(){
 
 function addListenerHitBox(){
     state.view.squares.forEach((square) => {
+        square.addEventListener("mousedown", () => {
+            if(square.id === state.values.hitPosition){
+                state.values.result++;
+                state.view.score.textContent = state.values.result;
+                state.values.hitPosition = null;
+            }
+        })
     })
 }
 
 
 function init() {  //funcao de inicio, muitos utlizam main tbm
     moveEnemy();
+    addListenerHitBox();
 }
 
 init();
