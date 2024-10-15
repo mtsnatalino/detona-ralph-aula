7100935bb76dde7f13e1738336b7351f0e5ad57c
mtsnatalino <matheuspedro982@gmail.com>
2024-10-14 22:43:07 -0300

    implementando o audio

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -44,9 +44,16 @@ function addListenerHitBox(){
                 state.values.result++;
                 state.view.score.textContent = state.values.result;
                 state.values.hitPosition = null;
+                playSound("hit");
             }
         })
     })
+}
+
+function playSound(audioName){
+    let audio = new Audio(`./src/audios/${audioName}.m4a`);
+    audio.volume = 0.2;
+    audio.play();
 }
 
 function countDown(){
